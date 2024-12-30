cbd3457de10375b8bde65790a394d1763e97ebe0
tingweihsu <tingweihsu.0131@gmail.com>
2024-12-30 17:42:35 +0800

    Final Version of insert comma project dealing with numbers

---
+++ Java_Beginner_Project_Lab/Project/src/InsertComma.java
@@ -0,0 +1,28 @@
+/**
+ * InsertComma
+ * @author Tim Hsu
+ * @version 20240906
+ */
+import java.util.Scanner;//import Scanner
+
+public class InsertComma {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);//import Scanner in method
+
+        //input number
+        System.out.print("Enter a comma-free number: ");
+        String number = in.next();
+
+        //split the number in the right place
+        String Last = number.substring((number.length()-3));
+        String First = number.substring(0,(number.length()-3));
+
+        //concatenate the number with commas in the right place
+        number = First+','+Last;
+
+        //print number with the commas
+        System.out.printf("Number with comma is: %s%n", number);
+        in.close();
+    }
+
+}
